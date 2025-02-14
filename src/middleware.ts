f0bd0380c454bfd16899fe20134ce36dab2955d6
Ruslan <info@mirasov.dev>
2025-02-14 01:05:59 +0100

    change middleware to custom

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,17 @@
-export { default } from 'next-auth/middleware';
+//export { default } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(req: NextRequest) {
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!session) {
+    return NextResponse.redirect(`${req.nextUrl.origin}/login?callbackUrl=${req.nextUrl.href}`);
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ['/', '/create-new-project'],
