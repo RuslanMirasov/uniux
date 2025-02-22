73c51b81cf285ae98b581878b112ab5a70a14e0a
Ruslan <info@mirasov.dev>
2025-02-23 00:14:12 +0100

    add rules to firebase

---
+++ src/app/api/auth/firebase/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth/next';
+import admin from '@/lib/firebase-admin';
+
+export async function GET(req: Request) {
+  const session = await getServerSession();
+  if (!session || !session?.user?.email) {
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+  }
+
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get('id');
+
+  if (!id) {
+    return NextResponse.json({ message: 'Missing user ID' }, { status: 400 });
+  }
+
+  const customToken = await admin.auth().createCustomToken(id);
+
+  return NextResponse.json({ token: customToken });
+}
--- src/lib/uploadAvatar.ts
+++ src/lib/uploadAvatar.ts
@@ -2,8 +2,23 @@ import { storage } from '@/lib/firebase';
 import { fetcher } from '@/lib/fetcher';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { optimizeImage } from './optimizeImage';
+import { getAuth, signInWithCustomToken } from 'firebase/auth';
+
+interface FirebaseToken {
+  token: string;
+}
 
 export const uploadAvatar = async (file: File, id: string) => {
+  const auth = getAuth();
+
+  const { token } = await fetcher<FirebaseToken>(`/api/auth/firebase?id=${id}`);
+
+  await signInWithCustomToken(auth, token);
+
+  if (!auth.currentUser) {
+    throw new Error('Firebase authentication failed');
+  }
+
   const compressedFile = await optimizeImage(file);
   const storageRef = ref(storage, `avatars/avatar_${id}.webp`);
   await uploadBytes(storageRef, compressedFile);
